e2fef9cb2a6d2d4cd55b571a00ac447bb70ca5e9
Ayush Ghetia <ayushghetia.2506@gmail.com>
2024-08-02 16:54:20 +0530

    final

--- file-explorer/src/App.js
+++ file-explorer/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tree from './components/Tree';
-// import './styles/App.css';
+import './styles/App.css';
 
 function App() {
   return (
--- file-explorer/src/service/api.js
+++ file-explorer/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    URL: 'https://localhost:5000/api'
+    baseURL: 'https://localhost:5000/api'
 });
 
 export default api;
